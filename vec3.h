c3aa08631bf4b0b5826c3e552fa032af2f8d9087
Andrew Ahrenkiel <andrewa3@iastate.edu>
2024-10-07 13:44:03 -0500

    added metal and materials header file

--- vec3.h
+++ vec3.h
@@ -46,6 +46,12 @@ public:
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+    }
+
     static vec3 random () {
         return vec3(random_double(), random_double(), random_double());
     }
@@ -72,8 +78,8 @@ inline vec3 operator-(const vec3& u, const vec3& v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline double operator*(const vec3& u, const vec3& v) {
-    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; // Dot product
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
@@ -102,6 +108,25 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        auto lensq = p.length_squared();
+        if (1e-160 < lensq && lensq <= 1) return p / sqrt(lensq);
+    }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    auto on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) return on_unit_sphere;
+    else return -on_unit_sphere;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    //Calculate projection of v onto n, then scale by 2 to get b, then subtract b from v to get the reflection
+    return v - 2*dot(v,n)*n;
 }
 
 #endif // VEC3_H
