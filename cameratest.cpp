18ee133a7961f9b2e7b007ff22b93db09a74e083
Andrew Ahrenkiel <andrewa3@iastate.edu>
2024-11-18 19:55:15 -0600

    added image texturing

--- cameratest.cpp
+++ cameratest.cpp
@@ -104,9 +104,32 @@ void checkered_spheres() {
     cam.render(world);
 }
 
+void external_img(){ 
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto earth = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(earth));
+}
+
 int main() {
-    switch(2){
+    switch(3){
         case 1: bouncing_spheres(); break;
         case 2: checkered_spheres(); break;
+        case 3: external_img(); break;
     }
 }
--- texture.h
+++ texture.h
@@ -3,6 +3,8 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "rtw_stb_image.h"
+
 
 class texture {
     public:
@@ -45,6 +47,30 @@ class checker_texture : public texture {
         double inv_scale;
         shared_ptr<texture> even;
         shared_ptr<texture> odd;    
+};
+
+class image_texture : public texture {
+    public:
+        image_texture(const char* filename) : image(filename) {}
+
+        color value(double u, double v, const point3& p) const override {
+            // return solid cyan if we don't have texture data
+            if (image.height() <= 0) return color(0, 1, 1);
+
+            //clamp input texture coord to [0,1] x [1,0]
+            u = interval(0,1).clamp(u);
+            v = 1.0 - interval(0,1).clamp(v); //fliv v to img coords
+
+            auto i = int(u * image.width());
+            auto j = int(v * image.height());
+            auto pixel = image.pixel_data(i, j);
+
+            auto color_scale = 1.0 / 255.0;
+            return color(color_scale * pixel[0], color_scale * pixel[1], color_scale * pixel[2]);
+        }
+
+        private:
+            rtw_image image;
 };
 
 #endif
